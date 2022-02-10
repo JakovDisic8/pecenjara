038e21ce5ce4f7678425afe5d37b6a4092b58bfb
Jakov <jakovdisic8@gmail.com>
2022-02-10 17:29:37 +0100

    new css
    dodat noci css

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
-// slider elements
-const slideImage=document.querySelectorAll(`.slide-image`);
-const slidesContainer=document.querySelector(`.slides-container`);
-const nextBtn=document.querySelector(`.next-btn`);
-const prevBtn=document.querySelector(`.prev-btn`);
-let slideWidth=slideImage[0].clientWidth;
-let slideNumber=slideImage.length;
-let curentSlide=0;
-// slider
-function init(){
-    //slideImage[0]=0
-    // slideImage[1]=100%
-    // slideImage[2]=200%
-    
-    slideImage.forEach((img,i)=>{
-        img.style.left=i*100+`%`;
-    });
-    slideImage[0].classList.add(`active`);
-}
-init();
-// next btn
-nextBtn.addEventListener(`click`,()=>{
-    curentSlide++;
-    goToSlide(curentSlide);
 
-});
-function goToSlide(){
-slidesContainer.style.transform=`translateX(-${slideWidth*slideNumber}px)`;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
 }
