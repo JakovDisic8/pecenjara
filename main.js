f9cbb42fde57099d981a96d891234ee1242f6b28
Jakov <jakovdisic8@gmail.com>
2022-02-16 20:13:56 +0100

    novi elementi i css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// img slider main page
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -22,4 +22,16 @@ function showSlides(n) {
   
   slides[slideIndex-1].style.display = "block";  
   
+}
+
+   // When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
 }
