cd45f33f820aefb96a4113866ba32aa48112f7e4
Jakov <jakovdisic8@gmail.com>
2022-02-20 20:06:58 +0100

    raspodela u foldere

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function showSlides(n) {
   
 }
 
-   // When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 window.onscroll = function() {scrollFunction()};
 
@@ -35,4 +35,7 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-50px";
   }
 }
+
+
+
 
